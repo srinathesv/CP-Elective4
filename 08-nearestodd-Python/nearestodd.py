ce492429e8d2c077e1d1044655535b335626fd64
Srinathesv <srinath.esv@msitprogram.net>
2021-08-29 16:31:00 +0530

    Done

--- 08-nearestodd-Python/nearestodd.py
+++ 08-nearestodd-Python/nearestodd.py
@@ -4,8 +4,13 @@
 # Note that the result must be an int, so nearestOdd(13.0) is the int 13, and not the float 13.0.
 
 
-
+import math
 def fun_nearestodd(n):
-	return 0
+	num = math.ceil(n)
+	if num%2==0:
+    		return num-1
+	else:
+    		return num
+
 
 
